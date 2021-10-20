59d947cea5d994257d8666a4f440765305e9f20a
Project-pong <91127063+Project-pong@users.noreply.github.com>
2021-10-20 18:11:13 +0200

    Add files via upload

---
+++ Arcade-Pong-V0.6-scoreboard/Pong project homescreen/Pong project homescreen/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pong_project_homescreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Image_MouseUp(object sender, RoutedEventArgs e)
+        {
+            string P1Naam = Player1_naam.Text;
+            string P2Naam = Player2_naam.Text;
+            
+            //Spel starten
+            ClassicPong game = new ClassicPong(P1Naam, P2Naam);
+            game.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void Image2_MouseUp(object sender, RoutedEventArgs e)
+        {
+            string P1Naam = Player1_naam.Text;
+            string P2Naam = Player2_naam.Text;
+
+            //Spel starten
+            NijntjePong game2 = new NijntjePong(P1Naam, P2Naam);
+            game2.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void Classic_Pong_MouseEnter(object sender, MouseEventArgs e)
+        {
+            border_classic.BorderBrush = Brushes.Red;
+        }
+
+        private void Classic_Pong_MouseLeave(object sender, MouseEventArgs e)
+        {
+            border_classic.BorderBrush = Brushes.Gray;
+        }
+
+        private void Nijntje_Pong_MouseEnter(object sender, MouseEventArgs e)
+        {
+            border_nijntje.BorderBrush = Brushes.Red;
+        }
+        private void Nijntje_Pong_MouseLeave(object sender, MouseEventArgs e)
+        {
+            border_nijntje.BorderBrush = Brushes.Gray;
+        }
+
+      
+    }
+}
